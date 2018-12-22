e7d19d094130b87b2f207df85528c3c55add672f
Bruno Maciel <brunomaciel.xc4@gmail.com>
2018-12-22 13:32:41 -0200

    Restruturacao do codigo

--- Semaforo_Iterativo/Semaforo_Iterativo.ino
+++ Semaforo_Iterativo/Semaforo_Iterativo.ino
@@ -1,95 +1,53 @@
 #include <LiquidCrystal.h>
 
-int carro_verde = 12;
-int carro_amarelo = 11;
-int carro_vermelho = 10;
-int ped_verde = 9;
-int ped_vermelho = 8;
+/* Definição dos pinos usados */
+int led_carroVerde = 12;
+int led_carroAmarelo = 11;
+int led_carroVermelho = 10;
+int led_pedVerde = 9;
+int led_pedVermelho = 8;
 int botao = 2;
 int buzzer = 4;
-int EstadoBotao = 0;
-unsigned long tempo_espera;
 LiquidCrystal lcd(0, 1, 3, 5, 6, 7);
 
-void setup()
-{
-  pinMode (carro_verde, OUTPUT);
-  pinMode (carro_amarelo, OUTPUT);
-  pinMode (carro_vermelho, OUTPUT);
-  pinMode (ped_verde, OUTPUT);
-  pinMode (ped_vermelho, OUTPUT);
-  pinMode (botao, INPUT);
-  pinMode (buzzer, OUTPUT);
-  lcd.begin(16, 2);
-  
-  digitalWrite (carro_verde, HIGH);
-  digitalWrite (ped_vermelho, HIGH);
-}
+/* Definição de variáveis auxiliares */
+int EstadoBotao = 0;
+unsigned long tempo_espera;
 
-void loop ()
-{
-  lcd.setCursor(0,0);
-  lcd.print("Aperte o botao"); //mostra no display a mensagem
-  lcd.setCursor(0,1);
-  lcd.print("para atravessar"); //"Aperte o botao para atravessa"
-  EstadoBotao = digitalRead(botao);
-  if (EstadoBotao == HIGH && millis()-tempo_espera>=20000) //condicao para acionamento imediato
-  {
-    lcd.clear();
-    lcd.setCursor(4,0);
-    lcd.print("Aguarde"); //exibe mensagem "aguarde"
-    aciona(); //mudanca de luzes
-  }
-  else if (EstadoBotao == HIGH && millis()-tempo_espera<20000) //condicao de espera pra acionamento
-  {
-    lcd.clear(); //limpa a mensagem "aperte um botao para atravessar
-    lcd.setCursor(4,0);
-    lcd.print("Aguarde"); //exibe mensagem "aguarde"
-    lcd.setCursor(7,1);
-    lcd.print("0");
-    mudatempo(7); delay(1000); //contagem de tempo para abrir o sinal
-    mudatempo(6); delay(1000);
-    mudatempo(5); delay(1000);
-    mudatempo(4); delay(1000);
-    mudatempo(3); delay(1000);
-    mudatempo(2); delay(1000);
-    mudatempo(1); delay(1000);
-    mudatempo(0);
-    aciona(); //mudanca de luzes
-  }
-}
 
-void aciona ()
-{
-  digitalWrite (carro_verde, LOW);
-  digitalWrite (carro_amarelo, HIGH); //sinal amarelo pra carros por 2seg
+void aciona () {
+  /* Ciclo de Abertura e Fechamento do Semaforo
+   * Semaforo para carros fecha -> Semaforo para pedestres abre
+   * Semaforo para pedestres fecha -> Semaforo para carros abre
+  */
+  digitalWrite (led_carroVerde, LOW);
+  digitalWrite (led_carroAmarelo, HIGH);  // sinal amarelo pra carros por 2seg
   delay(2000);
-  digitalWrite (carro_amarelo, LOW);
-  digitalWrite (carro_vermelho, HIGH); //sinal de carros muda para vermelho
+  digitalWrite (led_carroAmarelo, LOW);
+  digitalWrite (led_carroVermelho, HIGH); // sinal de carros muda para vermelho
   delay(1000);
-  digitalWrite (ped_vermelho, LOW);
+  digitalWrite (led_pedVermelho, LOW);    // sinal de pedestres abre 1seg após sinal de carros fechar
   atravessar();
   mudatempo(0); //sinal de pedestres ira fechar
-  digitalWrite (ped_verde, LOW);
-  digitalWrite (ped_vermelho, HIGH); //sinal de pedestres fecha
+  digitalWrite (led_pedVerde, LOW);
+  digitalWrite (led_pedVermelho, HIGH); //sinal de pedestres fecha
   lcd.clear();                          //retira a mensagem de atravessar
   lcd.setCursor(7,1);  lcd.print("00"); //e exibe apenas o tempo 00
   delay(1000);
-  digitalWrite (carro_vermelho, LOW);
-  digitalWrite (carro_verde, HIGH); //sinal de carros abre
+  digitalWrite (led_carroVermelho, LOW);
+  digitalWrite (led_carroVerde, HIGH); //sinal de carros abre
   tempo_espera = millis();
 }
 
-void atravessar()
-{
-  digitalWrite (ped_verde, HIGH); //sinal de pedestres abre
+void atravessar() {
+  /* Ciclo de Abertura e Fechamento do Semaforo de Pedestres */
+  digitalWrite (led_pedVerde, HIGH); // semaforo de pedestres abre
   lcd.setCursor(3,0);
-  lcd.print("Atravesse"); //exibe mensagem "Atrevesse"
+  lcd.print("Atravesse");     // exibe mensagem "Atrevesse"
   lcd.setCursor(7,1);
   lcd.print(0);
-  mudatempo(5); //exibe mensagem que falta 5 seg para fechar o sinal de pedestres
-  for (int y=0; y<15; y++) //luz verde pedestre aceso constante e buzzer apita por 3 seg
-  {
+  mudatempo(5);               //exibe mensagem que falta 5 seg para fechar o sinal de pedestres
+  for (int y=0; y<15; y++) {  //luz verde pedestre aceso constante e buzzer apita por 3 seg
     digitalWrite (buzzer, HIGH);
     delay(100);
     digitalWrite (buzzer, LOW);
@@ -99,22 +57,18 @@ void atravessar()
     if (y == 9)
     {    mudatempo(3);    } //falta 3seg para fechar o sinal de pedestres
   }
-  mudatempo(2); //falta 2seg para fechar o sinal de pedestres
-  for (int x=0; x<5; x++) //pisca luz verde de pedestre e buzzer acelerado por 2 seg
-  {
-    digitalWrite (ped_verde, LOW);
-    for (int z=0; z<2;z++)
-    { 
+  mudatempo(2); // falta 2seg para fechar o sinal de pedestres
+  for (int x=0; x<5; x++) { //pisca luz verde de pedestre e buzzer acelerado por 2 seg
+    digitalWrite (led_pedVerde, LOW);
+    for (int z=0; z<2;z++) { 
       digitalWrite (buzzer, HIGH);
       delay(50);
       digitalWrite (buzzer, LOW);
       delay(50);
     }
-    if (x == 2)
-    {    mudatempo(1);    } //falta 1seg para fechar o sinal de pedestres
-    digitalWrite (ped_verde, HIGH);
-    for (int z=0; z<2;z++)
-    { 
+    if (x == 2) {    mudatempo(1);    } //falta 1seg para fechar o sinal de pedestres
+    digitalWrite (led_pedVerde, HIGH);
+    for (int z=0; z<2;z++) { 
       digitalWrite (buzzer, HIGH);
       delay(50);
       digitalWrite (buzzer, LOW);
@@ -123,9 +77,56 @@ void atravessar()
   }
 }
 
-void mudatempo(int a) //funcao para exibir no display o tempo
-{
+void mudatempo(int a) {
+  /* Funcao que exibe no display o tempo */
   lcd.setCursor(8,1);
   lcd.print(a);
 }
 
+
+/***** Funções Setup e Loop *****/
+void setup() {
+  pinMode (led_carroVerde, OUTPUT);
+  pinMode (led_carroAmarelo, OUTPUT);
+  pinMode (led_carroVermelho, OUTPUT);
+  pinMode (led_pedVerde, OUTPUT);
+  pinMode (led_pedVermelho, OUTPUT);
+  pinMode (botao, INPUT);
+  pinMode (buzzer, OUTPUT);
+  lcd.begin(16, 2);
+  
+  digitalWrite (led_carroVerde, HIGH);
+  digitalWrite (led_pedVermelho, HIGH);
+}
+
+void loop () {
+  // exibe a mensagem no display "Aperte o botao para atravessa"
+  lcd.setCursor(0,0);
+  lcd.print("Aperte o botao");
+  lcd.setCursor(0,1);
+  lcd.print("para atravessar");
+  
+  EstadoBotao = digitalRead(botao);
+  if (EstadoBotao == HIGH && millis()-tempo_espera>=20000) {  // condicao para acionamento imediato
+    lcd.clear();
+    lcd.setCursor(4,0);
+    lcd.print("Aguarde"); // exibe mensagem "Aguarde"
+    aciona();             // mudanca de luzes
+  }
+  else if (EstadoBotao == HIGH && millis()-tempo_espera<20000) { //condicao de espera pra acionamento
+    lcd.clear();          // limpa a mensagem "aperte um botao para atravessar"
+    lcd.setCursor(4,0);
+    lcd.print("Aguarde"); // exibe mensagem "Aguarde"
+    lcd.setCursor(7,1);
+    lcd.print("0");
+    mudatempo(7); delay(1000); // contagem de tempo para abrir o sinal
+    mudatempo(6); delay(1000);
+    mudatempo(5); delay(1000);
+    mudatempo(4); delay(1000);
+    mudatempo(3); delay(1000);
+    mudatempo(2); delay(1000);
+    mudatempo(1); delay(1000);
+    mudatempo(0);
+    aciona(); // mudanca de luzes
+  }
+}
